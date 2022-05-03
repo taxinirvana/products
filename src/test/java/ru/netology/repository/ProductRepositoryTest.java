989a1848c8e2eaf1b351bb031c906d654eea8c70
Александр <taxinirvanaa@yandex.ru>
2022-05-03 11:09:39 +0500

    product

---
+++ src/test/java/ru/netology/repository/ProductRepositoryTest.java
@@ -0,0 +1,89 @@
+package ru.netology.repository;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.Book;
+import ru.netology.domain.NotFoundException;
+import ru.netology.domain.Product;
+import ru.netology.domain.Smartphone;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductRepositoryTest {
+    private ProductRepository repository = new ProductRepository();
+    Product one = new Book(1, "Secret", 15, "Ivanov");
+    Product two = new Book(2, "Animals", 20, "Petrov");
+    Product three = new Smartphone(3, "Phone", 100, "Russia");
+    Product four = new Smartphone(4, "New Phone", 120, "Germany");
+
+    @Test
+    public void shouldSaveAllProduct() {
+        repository.save(one);
+        repository.save(two);
+
+        Product[] expected = new Product[]{one, two};
+        Product[] actual = repository.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSaveAllProductOneElement() {
+        repository.save(one);
+
+        Product[] expected = new Product[]{one};
+        Product[] actual = repository.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSaveAllProductNoElement() {
+
+        Product[] expected = new Product[0];
+        Product[] actual = repository.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRemoveItem() {
+        int id = 3;
+        repository.save(one);
+        repository.save(two);
+        repository.save(three);
+        repository.save(four);
+
+        Product[] expected = new Product[]{one, two, four};
+        Product[] actual = repository.removeById(3);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRemoveItemOneElement() {
+        repository.save(two);
+
+        Product[] expected = new Product[0];
+        Product[] actual = repository.removeById(2);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldGiveMessage() {
+        int id = 5;
+        repository.save(one);
+        repository.save(two);
+        repository.save(three);
+        repository.save(four);
+
+        assertThrows(NotFoundException.class, () -> {
+            repository.removeById(5);
+        });
+    }
+
+    @Test
+    public void shouldGiveMessageOneElement() {
+        int id = 4;
+        repository.save(one);
+
+        assertThrows(NotFoundException.class, () -> {
+            repository.removeById(4);
+        });
+    }
+}
